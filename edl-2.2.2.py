ab90e00ed495346f3655e237bafdcd7d48045b68
mooncake96 <72810087+Pamella96@users.noreply.github.com>
2023-05-21 19:51:34 -0300

    edl

---
+++ edl-2.2.2.py
@@ -0,0 +1,17 @@
+# Exemplo 1: Amarrando um nome a um número inteiro
+idade = 25  # Nome: idade; Propriedade: valor inteiro; Tempo: tempo de execução
+
+# Exemplo 2: Amarrando um nome a uma string
+nome = "Alice"  # Nome: nome; Propriedade: valor string; Tempo: tempo de execução
+
+# Exemplo 3: Amarrando um nome a uma lista
+numeros = [1, 2, 3, 4, 5]  # Nome: numeros; Propriedade: lista de números; Tempo: tempo de execução
+
+# Exemplo 4: Amarrando um nome a um dicionário
+pessoa = {"nome": "João", "idade": 30, "cidade": "São Paulo"}  # Nome: pessoa; Propriedade: dicionário com informações pessoais; Tempo: tempo de execução
+
+# Exemplo 5: Amarrando um nome a uma função
+def somar(a, b):
+    return a + b
+
+funcao_somar = somar  # Nome: funcao_somar; Propriedade: função de soma; Tempo: tempo de execução
